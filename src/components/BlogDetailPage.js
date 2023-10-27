0a93dd05b950414f067b67ab2b486b77bafee7be
NijatS <nijatjs@code.edu.az>
2023-10-28 00:31:23 +0400

    add:Router

---
+++ src/components/BlogDetailPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BlogDetailPage = () => {
+  return <div>Blog Detail Page</div>;
+};
+
+export default BlogDetailPage;
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Header from "../components/Header";
+import HomePage from "../components/HomePage";
+import BlogListPage from "../components/BlogListPage";
+import BlogDetailPage from "../components/BlogDetailPage";
+import ContactPage from "../components/ContactPage";
+import NotFoundPage from "../components/NotFoundPage";
+import {
+  Route,
+  BrowserRouter,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom.min";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Switch>
+          <Route component={HomePage} path="/" exact />
+          <Route component={BlogListPage} path="/blogs" exact />
+          <Route component={BlogDetailPage} path="/blogs/:id" />
+          <Route component={ContactPage} path="/contact" />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
