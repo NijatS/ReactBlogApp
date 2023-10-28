f047e5a9b69a31fae0f0be3ba69955f2a0372e6f
NijatS <nijatjs@code.edu.az>
2023-10-28 12:04:33 +0400

    add:AutoId&AddBlog

--- src/index.js
+++ src/index.js
@@ -3,29 +3,26 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import AppRouter from "./routers/AppRouter";
 import { createStore, combineReducers } from "redux";
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
-
-const state = {
-  blogs: [
-    {
-      id: 1,
-      title: "Blog Title 1",
-      text: "Blog Text 1",
-      createdDate: NaN,
-    },
-  ],
-  auth: {
-    userId: "1",
-    username: "nijat",
-    email: "nicatsoltanli03@gmail.com",
-  },
-};
 
 const blogState = [];
 const authState = {};
 
+const addBlog = ({ title = "", text = "", createdDate = 0 }) => ({
+  type: "Add_Blog",
+  blog: {
+    id: uuidv4(),
+    title: title,
+    text: text,
+    createdDate: createdDate,
+  },
+});
+
 const blogReducer = (state = blogState, action) => {
   switch (action.type) {
+    case "Add_Blog":
+      return [...state, action.blog];
     default:
       return state;
   }
@@ -43,8 +40,18 @@ const store = createStore(
     auth: authReducer,
   })
 );
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
-console.log(store.getState());
+store.dispatch(addBlog({ title: "Blog Title 1", text: "Blog Text 1" }));
+store.dispatch(
+  addBlog({
+    title: "Blog Title 2",
+    text: "Blog Text 2",
+    createdDate: Date.now(),
+  })
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppRouter />);
